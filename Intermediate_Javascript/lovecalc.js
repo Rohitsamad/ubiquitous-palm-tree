07b5f044540c3fcd266591409fc291d0f4dba99d
Rohitsamad <rohitsamad4@gmail.com>
2021-07-12 17:24:58 +0530

    combining comparitive

--- Intermediate_Javascript/lovecalc.js
+++ Intermediate_Javascript/lovecalc.js
@@ -6,7 +6,12 @@ lovescore = Math.floor(lovescore) +1 ;
 
 if(lovescore >70){
     alert("Your love score is "+ lovescore + "% You have a perfect match");
-} else
+}
+if (lovescore>30 && lovescore<=70)
 {
     alert("Your love score is "+ lovescore + "%");
+}
+if (lovescore <= 30)
+{
+    alert("Your love score is "+ lovescore + "% you got together by oil nad water");
 }
