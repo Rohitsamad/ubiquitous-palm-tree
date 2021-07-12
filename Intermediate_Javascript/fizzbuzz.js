45757744b100ad993e40166ec7015a18e51226ab
Rohitsamad <rohitsamad4@gmail.com>
2021-07-12 19:30:09 +0530

    Control Statements: For Loops

--- Intermediate_Javascript/fizzbuzz.js
+++ Intermediate_Javascript/fizzbuzz.js
@@ -2,19 +2,18 @@ var output = [];
 var count = 1;
 
 function fizzbuzz() {
-    if(count % 3 === 0 && count % 5 ===0){
-        output.push("Fizzbuzz");
+    for(count =1; count< 101; count++) {
+        if(count % 3 === 0 && count % 5 ===0){
+            output.push("Fizzbuzz");
+        }
+        else if (count % 3 === 0) {
+            output.push("fizz");
+        } else if(count % 5 === 0){
+            output.push("Buzz");
+        } 
+        else {
+            output.push(count);
+        }
     }
-    else if (count % 3 === 0) {
-        output.push("fizz");
-    } else if(count % 5 === 0){
-        output.push("Buzz");
-    } 
-    else {
-        output.push(count);
-    }
-
-    count++;
-
     console.log(output);
 }
