9796cd2fea0edb4c0f74a7acda9f4d0b9800e926
Rohitsamad <rohitsamad4@gmail.com>
2021-07-12 17:16:40 +0530

    js if else condition

--- Intermediate_Javascript/lovecalc.js
+++ Intermediate_Javascript/lovecalc.js
@@ -3,4 +3,10 @@ prompt("What is there name?");
 
 var lovescore = Math.random() *100;
 lovescore = Math.floor(lovescore) +1 ;
-alert("Your love score is "+ lovescore + "%");
+
+if(lovescore >70){
+    alert("Your love score is "+ lovescore + "% You have a perfect match");
+} else
+{
+    alert("Your love score is "+ lovescore + "%");
+}
