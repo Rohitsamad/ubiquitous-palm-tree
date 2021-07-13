a35b81dee938294476a1b5a8a186a8abe60abdb0
Rohitsamad <rohitsamad4@gmail.com>
2021-07-13 16:30:14 +0530

    processing post request with body paser

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const B = require("body-parser");
 
 const app = express();
+app.use(B.urlencoded({extended: true}));
 
 app.get("/", function(request, response){
     response.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(request, response){
+
+    var num1 = Number(request.body.num1);
+    var num2 = Number(request.body.num2);
+
+    var result = num1+num2;
+
+    response.send("The result of the calcilation is " + result);
 });
 
 app.listen(3000, function(){
