e2351f9c5131fef87cc7038570183fcd81258ed8
Rohitsamad <rohitsamad4@gmail.com>
2021-07-13 22:40:14 +0530

    weather project compleat

--- Weatherproject/app.js
+++ Weatherproject/app.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const https = require("https");
-const { url } = require("inspector");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res){
-    
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=london&appid=99b5fd3aad5a238aa4ee2019caf2c5cc";
+    res.sendFile(__dirname+"/index.html");
+});
+
+app.post("/", function(req, res){
+    const city = req.body.cityInput;
+    const apikey = "99b5fd3aad5a238aa4ee2019caf2c5cc";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apikey;
 
     https.get(url, function(response){
         console.log(response.statusCode);
@@ -15,13 +22,26 @@ app.get("/", function(req, res){
             const weatherData = JSON.parse(data)
             const temp = weatherData.main.temp
             const desc = weatherData.weather[0].description
-            
+            const icon = weatherData.weather[0].icon
+            const url = "http://openweathermap.org/img/wn/"+ icon +"@2x.png";
+            res.write("<h1>the weather is "+ desc + "</h1>");
+            res.write("<h2>The temperature in "+city+" is" + temp+ "</h2>");
+            res.write("<img src="+url + ">");
+            res.send();
         })
     })
-    res.send("server is up");
 })
+    
 
 
+
+
+/*
+app.get("/", function(req, res){
+    
+    
+
+*/
 app.listen(3000, function() {
-    console.log("server is running on port 3000.");
+    
 })
