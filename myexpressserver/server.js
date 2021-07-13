32679eeac1d805307097afb3be719a8cd82587f1
Rohitsamad <rohitsamad4@gmail.com>
2021-07-13 15:10:43 +0530

    working with route

--- myexpressserver/server.js
+++ myexpressserver/server.js
@@ -6,6 +6,14 @@ app.get("/", function(request, response){
     response.send("hello");
 });
 
+app.get("/contact", function(request, response){
+    response.send("contact me at rohitsamad4@gmail.com");
+});
+
+app.get("/about", function(request, response){
+    response.send("I am developer and a coder");
+});
+
 app.listen(3000, function(){
     console.log("server started on port 3000");
 });
