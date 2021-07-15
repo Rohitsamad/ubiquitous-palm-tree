cce6a8b96a808b719cd7d900d568bf9045aa1884
Rohitsamad <rohitsamad4@gmail.com>
2021-07-15 20:31:48 +0530

    Keeper app

--- keeper-app/src/components/Note.jsx
+++ keeper-app/src/components/Note.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Note() {
     return (
         <div className="note">
-            <h1>This is title</h1>
+            <h1>This is a title</h1>
             <p>This is content</p>
         </div>
     )
