ae6d60b988e4b51fb6576c1e23a77ce967bc0d3e
Rohitsamad <rohitsamad4@gmail.com>
2021-07-16 16:38:54 +0530

    react dev tool

---
+++ react-devtools/src/components/Detail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Detail(props) {
+    return (
+        <div>
+            <p className="info">{props.detaillnfo}</p>
+        </div>
+    )
+}
+
+export default Detail;
