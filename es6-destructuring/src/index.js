f039d5db84d57e588a10e44b432df5a58ee14777
Rohitsamad <rohitsamad4@gmail.com>
2021-07-17 11:39:10 +0530

    es6 destructuring challenge

--- es6-destructuring/src/index.js
+++ es6-destructuring/src/index.js
@@ -1,23 +1,34 @@
 // CHALLENGE: uncomment the code below and see the car stats rendered
-// import React from "react";
-// import ReactDOM from "react-dom";
 
-// ReactDOM.render(
-//   <table>
-//     <tr>
-//       <th>Brand</th>
-//       <th>Top Speed</th>
-//     </tr>
-//     <tr>
-//       <td>{tesla.model}</td>
-//       <td>{teslaTopSpeed}</td>
-//       <td>{teslaTopColour}</td>
-//     </tr>
-//     <tr>
-//       <td>{honda.model}</td>
-//       <td>{hondaTopSpeed}</td>
-//       <td>{hondaTopColour}</td>
-//     </tr>
-//   </table>,
-//   document.getElementById("root")
-// );
+import animals from "./data";
+import cars from "./practice";
+import React from "react";
+import ReactDOM from "react-dom";
+
+const [honda, tesla] = cars;
+
+const {speedStats: {topSpeed: hondaTopSpeed}} = honda;
+const {speedStats: {topSpeed: teslaTopSpeed}} = tesla;
+
+const {coloursByPopularity : [hondaTopColour]} = honda;
+const {coloursByPopularity : [teslaTopColour]} = tesla;
+
+ ReactDOM.render(
+   <table>
+     <tr>
+       <th>Brand</th>
+       <th>Top Speed</th>
+     </tr>
+     <tr>
+       <td>{tesla.model}</td>
+       <td>{teslaTopSpeed}</td>
+       <td>{teslaTopColour}</td>
+     </tr>
+     <tr>
+       <td>{honda.model}</td>
+       <td>{hondaTopSpeed}</td>
+       <td>{hondaTopColour}</td>
+     </tr>
+   </table>,
+   document.getElementById("root")
+ );
